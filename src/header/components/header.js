cf7b3f0cb547f7c744693581daa86dd0c9e922a4
mw <g33456@gmail.com>
2016-08-05 15:16:16 +0800

    ...

--- src/header/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Nav from '../containers/nav'
-import VisibleTodoList from '../containers/VisibleTodoList'
-
-const Header = () => (
-    <Nav />
-)
-
-export default App
